6072a412c52a710196f392d2378d88048e25653b
Ari Kamarchevakul <snowbearypie@gmail.com>
2021-04-08 10:00:55 -0500

    initial commit

---
+++ text-generator.js
@@ -0,0 +1,8 @@
+
+document.getElementById("button").addEventListener("click", async function() {
+  const rnn = ml5.charRNN('models/CRTranscript/');
+  await rnn.generate({ seed: 'Matt', length: 400}, (err, results) => {
+    let text = results;
+    document.getElementById('text-generator').innerHTML=text.sample
+  });
+});
